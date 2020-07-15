74902801172e95a585bb90d346b369e63e4410fb
Kirill Lastovirya <kirill.lastovirya@gmail.com>
2020-07-16 09:09:11 +1000

    Fixed compilation errors

--- src/main.rs
+++ src/main.rs
@@ -11,13 +11,15 @@ use crate::limit_detector::LimitDetector;
 use crate::screen::Screen;
 use crate::renderer::Renderer;
 use crate::parameters::Parameters;
+use crate::types::Function;
+
+const CHARS: &str = " .`:,;'_^\"></-!~=)(|j?}{ ][ti+l7v1%yrfcJ32uIC$zwo96sngaT5qpkYVOL40&mG8*xhedbZUSAQPFDXWK#RNEHBM@";
+const FORMULA: &Function = &| z | z*z + Complex::new(-0.75, 0.1);
 
 fn main() {
     println!("Fractazoid a Fractal Explorer v1.0 [2020-07-15]");
     println!("==================================");
 
-    let chars = " .`:,;'_^\"></-!~=)(|j?}{ ][ti+l7v1%yrfcJ32uIC$zwo96sngaT5qpkYVOL40&mG8*xhedbZUSAQPFDXWK#RNEHBM@";
-    let formula = | z | z*z + Complex::new(-0.75, 0.1);
     let params = Parameters::build(
         // Complex plane
         -1.5,
@@ -30,8 +32,8 @@ fn main() {
         80,
         20,
 
-        &formula,
-        chars
+        FORMULA,
+        CHARS
     );
 
     let limit_detector = LimitDetector::from(&params);
